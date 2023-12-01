96550044f91d40ff585a48fd2c75256fe65ebf80
Piyush <piyushgk1@gmail.com>
2023-12-01 23:26:56 +0100

    feat: plz work

--- generate_games.py
+++ generate_games.py
@@ -8,7 +8,9 @@ join_tokens = ['canada','delta','united','frontier','sw','alaska','american','sp
 games = []
 with open('games.json', 'w+') as f:
     for join_token in join_tokens:
-        random.shuffle(players)
-        random.shuffle(tokens)
-        games.append({"joinToken":join_token, "players":players,"tokens":tokens})
+        n_players = [] + players
+        random.shuffle(n_players)
+        n_tokens = [] + tokens
+        random.shuffle(n_tokens)
+        games.append({"joinToken":join_token, "players":n_players,"tokens":n_tokens})
     f.write(json.dumps(games))
